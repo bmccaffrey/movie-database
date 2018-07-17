b8ea825b6bcf8a859f97c6eae3614a265bf1b3e3
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-17 17:07:10 -0500

    iterate over and create div for each movie

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        
+        {movies.map(movie => (
+          <div key={movie.id}>
+            {movie.title}
+          </div>
+          ))}
       </div>
     );
   }
