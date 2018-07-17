57993e542dd9af42785db30ff13128a93312c54b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-17 12:40:36 -0500

    add inline conditional

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This should show and hide</p>
+        {this.state.toggle && <p>This should show and hide</p>}
         <button>Show / Hide</button>
       </div>
     );
