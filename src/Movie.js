238db4b5ba3d34525b3824cd159ddba5250f3767
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-18 00:43:32 -0500

    add default prop for description

--- src/Movie.js
+++ src/Movie.js
@@ -4,16 +4,20 @@ import PropTypes from 'prop-types';
 export default class Movie extends Component {
   static propTypes = {
     movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      desc: PropTypes.string.isRequired
-    })
+      title: PropTypes.string.isRequired
+    }),
+    desc: PropTypes.string
+  };
+
+  static defaultProps = {
+    desc: 'Description is not available'
   };
 
   render() {
     return (
       <div>
         <h3>{this.props.movie.title}</h3>
-        <p>{this.props.movie.desc}</p>
+        <p>{this.props.desc}</p>
       </div>
     );
   }
