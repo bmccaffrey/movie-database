be878361809118b155c2e7df45cc9e911a5ff5ba
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-17 23:17:22 -0500

    fix format

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 
 import Movie from './Movie';
 
-const movies = [{
+const movies = [
+  {
     id: 1,
     title: 'The Empire Strikes Back'
-  }, {
+  },
+  {
     id: 2,
     title: 'Forest Gump'
-  }, {
+  },
+  {
     id: 3,
     title: 'Fight Club'
   }
@@ -23,7 +26,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
