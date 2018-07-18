816c514e7f246fd3946eaaf8db7063252d02139d
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-18 00:14:03 -0500

    set PropTypes

--- src/Movie.js
+++ src/Movie.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.object
+  };
+
   render() {
     return (
       <div>
         <h3>{this.props.movie.title}</h3>
+        <p>{this.props.movie.desc}</p>
       </div>
     );
   }
