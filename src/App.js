159a5f075be025879f1072037781aefa9d568788
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-18 04:22:59 -0500

    output data to Movie component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import Movie from './Movie';
-
-const movies = [
-  {
-    id: 1,
-    title: 'The Empire Strikes Back'
-  },
-  {
-    id: 2,
-    title: 'Forest Gump'
-  },
-  {
-    id: 3,
-    title: 'Fight Club'
-  }
-];
 
 class App extends Component {
   state = {
@@ -30,7 +14,6 @@ class App extends Component {
         'https://api.themoviedb.org/3/discover/movie?api_key=ececdbefc89c980fc44d7922eb16239f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
       );
       const movies = await res.json();
-      console.log(movies);
       this.setState({
         movies: movies.results
       });
@@ -40,14 +23,12 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.movies);
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
