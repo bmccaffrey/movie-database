2a71caa4a78d710e30f94afa7ab3d840deaac346
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-19 17:11:49 -0500

    display backdrop

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'https://image.tmdb.org/t/p/w1280';
+
 class MovieDetail extends Component {
   constructor() {
     super();
@@ -25,11 +28,14 @@ class MovieDetail extends Component {
   }
 
   render() {
+    const { movie } = this.state;
     return (
       <div>
-        <h1>{this.state.movie.title}</h1>
-        <h3>{this.state.movie.release_date}</h3>
-        <p>{this.state.movie.overview}</p>
+        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
+        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        <h1>{movie.title}</h1>
+        <h3>{movie.release_date}</h3>
+        <p>{movie.overview}</p>
       </div>
     );
   }
