036e783f31394d5409309a6f1cd4c4f1c19cfd51
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-19 17:34:03 -0500

    import Poster into Movie Detail

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Poster } from './Movie';
 
 const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = 'https://image.tmdb.org/t/p/w1280';
@@ -32,7 +33,7 @@ class MovieDetail extends Component {
     return (
       <div>
         <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
         <h1>{movie.title}</h1>
         <h3>{movie.release_date}</h3>
         <p>{movie.overview}</p>
