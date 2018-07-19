aaad06603536ad927c1336b4a2ee0e6858d407cf
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-19 09:26:28 -0500

    transform app into functional stateless component

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,5 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/jsx-filename-extension': 0,
-    'react/jsx-one-expression-per-line': 0,
   },
 };
--- src/App.js
+++ src/App.js
@@ -2,45 +2,25 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Movie from './Movie';
+import MoviesList from './MoviesList';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [],
-    };
-  }
-
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?api_key=ececdbefc89c980fc44d7922eb16239f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
-      );
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Route path="/test" component={Test} />
-          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Switch>
+        <Route path="/test" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
 
-const Test = () => <h1>TEST</h1>;
+const Test = () => (
+  <h1>
+TEST
+  </h1>
+);
