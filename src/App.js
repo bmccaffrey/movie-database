24bcdb660d04d36aadee8d7df7bd683a40cc5528
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-19 09:51:48 -0500

    allow navigation to home via header

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const App = () => (
   <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
       </header>
       <Switch>
         <Route exact path="/" component={MoviesList} />
